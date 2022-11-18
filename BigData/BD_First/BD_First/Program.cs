5bc7891e81b3a362e012957399efe26b7cb3bc57
CoalescedConsciousness <Santiak@kobuta.me>
2022-11-18 09:13:14 +0100

    [IMP] Added further clarifying comments where deemed appropriate

--- BigData/BD_First/BD_First/Program.cs
+++ BigData/BD_First/BD_First/Program.cs
@@ -9,12 +9,16 @@ var builder = WebApplication.CreateBuilder(args);
 var baseAddress = "https://localhost:7292/";
 
 #region Builder
+// Add DB and Services
 builder.Services.AddDbContext<BD_FirstContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("BD_FirstContext") ?? throw new InvalidOperationException("Connection string 'BD_FirstContext' not found.")));
 builder.Services.AddScoped<WeatherService>();
+
+// Enable use of Endpoints for WebAPI calls
 builder.Services.AddMvc(options => options.EnableEndpointRouting = false)
                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
 
+// Add clients used for outgoing API calls
 builder.Services.AddHttpClient();
 builder.Services.AddHttpClient("localapi", client =>
 {
@@ -35,13 +39,7 @@ builder.Services.AddScoped<HttpClient>(sp => {
 
 var app = builder.Build();
 
-// ApiKey = "ceeaa353-a263-4f66-b29d-9a14a3724a38";
-// curl - X GET "https://dmigw.govcloud.dk/v2/climateData" - H "X-Gravitee-Api-Key: ceeaa353-a263-4f66-b29d-9a14a3724a38"
-// Municipality (Sorø): 0340
-// Ex: https://dmigw.govcloud.dk/v2/climateData/collections/municipalityValue/items?municipalityId=0340&api-key=ceeaa353-a263-4f66-b29d-9a14a3724a38
-
-//app.MapGet("/", () => "Hello World!");
-
+// Add support for Routing used for API call functionality.
 app.UseRouting();
 app.UseMvcWithDefaultRoute();
 
