4b7b1678ea9b1c352e7a9d573de1ac382830ffd8
CoalescedConsciousness <Santiak@kobuta.me>
2022-11-28 15:06:31 +0100

    [WIP] Uploaded end-of-day exercise progress

--- IoT/blink_test/blink_test.ino
+++ IoT/blink_test/blink_test.ino
@@ -1,30 +1,46 @@
 #include "alphabet.h"
 #define PIN_LED 2
 
+String response;
 
 void setup() 
 {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(PIN_LED, OUTPUT);
   Serial.begin(9600);
+
 }
 
 // the loop function runs over and over again forever
 void loop() 
 {
+  //  while(Serial.available()) {
+
+  //   a= Serial.readString();// read the incoming data as string
+
+  //   Serial.println(a); 
+  //  }
   
-  String response = Serial.readString();
-  decipherString(response);
-  s(PIN_LED);
-  o(PIN_LED);
-  s(PIN_LED);
-  delay(2000);
+  while(Serial.available())
+  {
+    response = Serial.readString();
+    decipherString(response);
+      
+    // s(PIN_LED);
+    // o(PIN_LED);
+    // s(PIN_LED);
+    // delay(2000);
+  }
+
 }
 
 void decipherString(String input)
 {
-  for (int i = 0; i >= input.length(); i++) 
+  Serial.print(input.length());
+  for (int i = 0; i <= input.length(); i++) 
   {
-    Serial.println(input);
+    // Call morse letter
+    Serial.println(i);
+    Serial.print(input[i]);
   }
 }
