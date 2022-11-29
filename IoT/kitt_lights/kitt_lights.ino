0a307770e99008167dab0da9951581584dde7328
CoalescedConsciousness <Santiak@kobuta.me>
2022-11-29 12:14:10 +0100

    [FIX] Reverted overwritten code; Kitt-lights using integer based iteration loops

--- IoT/kitt_lights/kitt_lights.ino
+++ IoT/kitt_lights/kitt_lights.ino
@@ -1,14 +1,29 @@
-#define LED_1 2
-#define SWITCH_1 3
+#define LEDS 4
+#define START 2
 
-void setup()
-{
-  pinMode(LED_1, OUTPUT);
-  pinMode(SWITCH_1, INPUT);
+void setup() {
+  for (int i = 2; i < LEDS + 2; i++)
+  {
+    pinMode(i, OUTPUT);
+  }
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.println(digitalRead(SWITCH_1));
-  digitalRead(SWITCH_1) ? digitalWrite(LED_1, HIGH) : digitalWrite(LED_1, LOW);
+  for (int i = START; i < (LEDS + START); i++)
+  {
+    blink(i);
+  }
+  for (int i = (LEDS + START); i > START; i--)
+  {
+    blink(i);
+  }
+}
+
+void blink(int led)
+{
+    digitalWrite(led, HIGH);
+    delay(100);
+    digitalWrite(led, LOW);
+    delay(20);
 }
