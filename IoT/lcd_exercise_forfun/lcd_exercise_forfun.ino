39dd807345dc53b749985e716b92318ff95b9131
CoalescedConsciousness <Santiak@kobuta.me>
2022-11-29 14:53:43 +0100

    [IMP] Added example of potentiometer controlled timing on LED flash

--- IoT/lcd_exercise_forfun/lcd_exercise_forfun.ino
+++ IoT/lcd_exercise_forfun/lcd_exercise_forfun.ino
@@ -1,7 +1,23 @@
 #include <LiquidCrystal.h>
+#define POTENT A5
+#define LED_1 8
+int STATE = LOW;
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
+void setup()
+{
+  pinMode(LED_1, OUTPUT);
+  Serial.begin(9600);
+}
+void loop() {
+  int res = analogRead(POTENT);
+  Serial.println(res);
+  delay(res);
+  digitalWrite(LED_1, STATE);
+  STATE = !STATE;
+
+}
 byte customChar0[8] =
 {
   0b00000,
@@ -98,35 +114,27 @@ byte customChar7[8] =
   0b00000
 };
 
-void setup() {
-  lcd.createChar(0, customChar0);
-  lcd.createChar(1, customChar1);
-  lcd.createChar(2, customChar2);
-  lcd.createChar(3, customChar3);
-  lcd.createChar(4, customChar4);
-  lcd.createChar(5, customChar5);
-  lcd.createChar(6, customChar6);
-  lcd.createChar(7, customChar7);
-  lcd.begin(16, 2);
-  lcd.write(byte(0));
-  lcd.write(byte(1));
-  lcd.write(byte(4));
-  lcd.write(byte(5));
-  lcd.setCursor(0, 1);
-  lcd.write(byte(2));
-  lcd.write(byte(3));
+// void setup() {
+//   lcd.createChar(0, customChar0);
+//   lcd.createChar(1, customChar1);
+//   lcd.createChar(2, customChar2);
+//   lcd.createChar(3, customChar3);
+//   lcd.createChar(4, customChar4);
+//   lcd.createChar(5, customChar5);
+//   lcd.createChar(6, customChar6);
+//   lcd.createChar(7, customChar7);
+//   lcd.begin(16, 2);
+//   lcd.write(byte(0));
+//   lcd.write(byte(1));
+//   lcd.write(byte(4));
+//   lcd.write(byte(5));
+//   lcd.setCursor(0, 1);
+//   lcd.write(byte(2));
+//   lcd.write(byte(3));
 
  
-  lcd.write(byte(6));
-  lcd.write(byte(7));
-}
+//   lcd.write(byte(6));
+//   lcd.write(byte(7));
+// }
 
 
-void loop() {
-  // lcd.setCursor(0, 1);
-  // lcd.print(millis() / 1000);
-  
-  // lcd.setCursor(5, 1);
-  // lcd.print((char)223); // Unicode for Degree-symbol.
-
-}
